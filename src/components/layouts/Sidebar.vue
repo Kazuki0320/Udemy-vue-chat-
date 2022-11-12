25b38b6685f7c372734a78d6a5747750759e066a
Kazuki.T <bad0320@icloud.com>
2022-11-12 10:33:53 +0900

    ユーザーアイコンを変更

--- src/components/layouts/Sidebar.vue
+++ src/components/layouts/Sidebar.vue
@@ -1,85 +1,95 @@
-<template>
-	<v-navigation-drawer
-		v-model="drawer"
-		app
-	>
-		<v-sheet
-		color="grey lighten-4"
-		class="pa-4"
+	<template>
+		<v-navigation-drawer
+			v-model="drawer"
+			app
 		>
-		<v-avatar
-			class="mb-4"
-			color="grey darken-1"
-			size="64"
-		></v-avatar>
-  
-		<div class="username">{{ auth && auth.displayName }}</div>
-		</v-sheet>
-  
-		<v-divider></v-divider>
-  
-		<v-list>
-		<v-list-item
-			v-for="[icon, text, to] in links"
-			:key="icon"
-			:to="to"
-			link
-		>
-			<v-list-item-icon>
-			<v-icon>{{ icon }}</v-icon>
-			</v-list-item-icon>
-  
-			<v-list-item-content>
-			<v-list-item-title>{{ text }}</v-list-item-title>
-			</v-list-item-content>
-		</v-list-item>
+			<v-sheet
+			color="grey lighten-4"
+			class="pa-4"
+			>
 
-		<v-list-item @click="logout">
-			<v-list-item-icon>
-				<v-icon color="blue">
-					mdi-logout
+			<v-avatar color="indigo">
+				<input type="file"
+							ref="fileInput"
+							accept="image/jpeg, image.jpg, image.png"
+							style="display: none">
+				<v-icon dark
+							@click="changeIcon">
+					mdi-account-circle
 				</v-icon>
-			</v-list-item-icon>
-			<v-list-item-content>
-				<v-list-item-title>Logout</v-list-item-title>
-			</v-list-item-content>
-		</v-list-item>
+			</v-avatar>
+	
+			<div class="username">{{ auth && auth.displayName }}</div>
+			</v-sheet>
+	
+			<v-divider></v-divider>
+	
+			<v-list>
+			<v-list-item
+				v-for="[icon, text, to] in links"
+				:key="icon"
+				:to="to"
+				link
+			>
+				<v-list-item-icon>
+				<v-icon>{{ icon }}</v-icon>
+				</v-list-item-icon>
+	
+				<v-list-item-content>
+				<v-list-item-title>{{ text }}</v-list-item-title>
+				</v-list-item-content>
+			</v-list-item>
 
-		</v-list>
-	</v-navigation-drawer>
-</template>
+			<v-list-item @click="logout">
+				<v-list-item-icon>
+					<v-icon color="blue">
+						mdi-logout
+					</v-icon>
+				</v-list-item-icon>
+				<v-list-item-content>
+					<v-list-item-title>Logout</v-list-item-title>
+				</v-list-item-content>
+			</v-list-item>
 
-<script>
-import firebase from "@/firebase/firebase"
+			</v-list>
+		</v-navigation-drawer>
+	</template>
 
-export default {
-mounted() {
-	this.auth = JSON.parse(sessionStorage.getItem('user'))
-},
-data: () => ({
-	drawer: null,
-	links: [
-	['mdi-inbox-arrow-down', 'Inbox', '/'],
-	['mdi-send', 'Send', '/about'],
-	['mdi-delete', 'Trash', '/about'],
-	['mdi-alert-octagon', 'Spam', '/about'],
-	],
-	auth: null
-}),
-methods: {
-	logout() {
+	<script>
+	import firebase from "@/firebase/firebase"
+
+	export default {
+	mounted() {
+		this.auth = JSON.parse(sessionStorage.getItem('user'))
+	},
+	data: () => ({
+		drawer: null,
+		links: [
+		['mdi-inbox-arrow-down', 'Inbox', '/'],
+		['mdi-send', 'Send', '/about'],
+		['mdi-delete', 'Trash', '/about'],
+		['mdi-alert-octagon', 'Spam', '/about'],
+		],
+		auth: null
+	}),
+	methods: {
+		logout() {
 			console.log("logout call");
 			firebase.auth()
-			.signOut()
-			.then(() => {
-				localStorage.message = "ログアウトに成功しました"
-				console.log("ログアウトに成功しました");
-				this.$router.push('/login')
-			})
-			.catch((error) => {
-				console.log(error);
-			})
+				.signOut()
+				.then(() => {
+					localStorage.message = "ログアウトに成功しました"
+					console.log("ログアウトに成功しました");
+					this.$router.push('/login')
+				})
+				.catch((error) => {
+					console.log(error);
+				})
+		},
+		changeIcon() {
+			console.log("changeIcon call");
+			this.$refs.fileInput.click()
 		}
-	}
+	} 
 }
 </script>
